5b02f595e48e3d6512d5a42e18d4e7ff1e356d21
Vlad <vlad.ogay@nrg-soft.com>
2012-11-28 02:54:32 +0400

    Engineering Tools navigation

---
+++ MvcApplication1/MvcApplication1/Areas/EngineeringTools/Controllers/MesoWindController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Areas.EngineeringTools.Controllers
+{
+    public class MesoWindController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Database()
+        {
+            return View();
+        }
+        public ActionResult CurrentData()
+        {
+            return View();
+        }
+        public ActionResult VelocityFreq()
+        {
+            return View();
+        }
+        public ActionResult FreqPerSector()
+        {
+            return View();
+        }
+        public ActionResult MeanVelocityPerSector()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcApplication1/MvcApplication1/Areas/EngineeringTools/Controllers/WakeSimulationController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Areas.EngineeringTools.Controllers
+{
+    public class WakeSimulationController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GeneralProperties()
+        {
+            return View();
+        }
+
+        public ActionResult TurbineProperties()
+        {
+            return View();
+        }
+
+        public ActionResult Simulation()
+        {
+            return View();
+        }
+    }
+}
