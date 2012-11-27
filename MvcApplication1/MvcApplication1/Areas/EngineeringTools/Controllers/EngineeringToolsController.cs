871f266286a9db57543fe509e1ce6c7acd2062d9
Vlad <vlad.ogay@nrg-soft.com>
2012-11-28 00:13:48 +0400

    Areas: CFD and EngineeringTools

--- MvcApplication1/MvcApplication1/Areas/EngineeringTools/Controllers/EngineeringToolsController.cs
+++ MvcApplication1/MvcApplication1/Areas/EngineeringTools/Controllers/EngineeringToolsController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace MvcApplication1.Controllers
+namespace MvcApplication1.Areas.EngineeringTools.Controllers
 {
     public class EngineeringToolsController : Controller
     {
